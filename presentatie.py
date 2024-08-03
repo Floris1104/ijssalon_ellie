853f9ecb9e880ddf904d87e132269b7269470257
Floris Ouwerkerk <floris.ouwerkerk@gmail.com>
2024-08-03 16:36:29 +0200

    les 10

---
+++ presentatie.py
@@ -0,0 +1,6 @@
+def presenteer(dictionary, totaal):
+    dictionary = inkomsten
+    totaal = sum(dictionary.values)
+    print(dictionary)
+    print("==========================")
+    print(f"totaal: {totaal}")
